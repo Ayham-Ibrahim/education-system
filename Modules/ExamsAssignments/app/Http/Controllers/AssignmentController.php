df2d513e7fe8118a888ca735018eacb70883ff8d
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2024-10-01 12:07:45 +0300

    request validation

---
+++ Modules/ExamsAssignments/app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\ExamsAssignments\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\CourseManagement\Models\Subject;
+use Modules\ExamsAssignments\Models\Assignment;
+use Modules\ExamsAssignments\Http\Requests\AssignmentRequests\StoreAssignmentRequest;
+use Modules\ExamsAssignments\Http\Requests\AssignmentRequests\UpdateAssignmentRequest;
+
+class AssignmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index($subjectId)
+    {
+        $assignments = Assignment::where('subject_id', $subjectId)->get();
+        return response()->json($assignments);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAssignmentRequest $request,$subjectId)
+    {
+        $subject = Subject::findOrFail($subjectId);
+        $assignment = $subject->assignments()->create($request->validated());
+        return response()->json($assignment, 201);
+
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show(Assignment $assignment)
+    {
+        $assignment->load('questions');
+        return response()->json($assignment); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAssignmentRequest $request,Assignment $assignment)
+    {
+        $assignment->update(array_filter($request->validated()));
+        return response()->json($assignment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Assignment $assignment)
+    {
+        
+        $assignment->delete();
+        return response()->json('deleted seccessfully');
+    }
+}
--- Modules/ExamsAssignments/app/Http/Controllers/ExamController.php
+++ Modules/ExamsAssignments/app/Http/Controllers/ExamController.php
@@ -5,11 +5,82 @@ namespace Modules\ExamsAssignments\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Modules\ExamsAssignments\Models\Exam;
+use Modules\CourseManagement\Models\Subject;
 use Modules\ExamsAssignments\Models\Question;
 use Modules\ExamsAssignments\Models\StudentAnswer;
+use Modules\ExamsAssignments\Http\Requests\ExamRequests\StoreExamRequest;
+use Modules\ExamsAssignments\Http\Requests\ExamRequests\UpdateExamRequest;
 
 class ExamController extends Controller
 {
+    /**
+     * Summary of store
+     * @param \Modules\ExamsAssignments\Http\Requests\ExamRequests\StoreExamRequest $request
+     * @param mixed $subjectId
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function store(StoreExamRequest $request, $subjectId)
+    {
+        $subject = Subject::findOrFail($subjectId);
+        $exam = $subject->exams()->create($request->validated());
+        return response()->json($exam, 201);
+    }
+
+
+    /**
+     * Summary of index
+     * @param mixed $subjectId
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function index($subjectId)
+    {
+        $exams = Exam::where('subject_id', $subjectId)->get();
+        return response()->json($exams);
+    }
+
+
+    /**
+     * Summary of show
+     * @param \Modules\ExamsAssignments\Models\Exam $exam
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function show(Exam $exam)
+    {
+        $exam->load('questions');
+        return response()->json($exam);
+    }
+
+    /**
+     * Summary of update
+     * @param \Modules\ExamsAssignments\Http\Requests\ExamRequests\UpdateExamRequest $request
+     * @param \Modules\ExamsAssignments\Models\Exam $exam
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateExamRequest $request, Exam $exam)
+    {
+        $exam->update(array_filter($request->validated()));
+        return response()->json($exam);
+    }
+
+    /**
+     * Summary of destroy
+     * @param \Modules\ExamsAssignments\Models\Exam $exam
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function destroy(Exam $exam)
+    {
+        $exam->delete();
+        return response()->json(null, 204);
+    }
+    
+    
+    /**
+     * Summary of submitAnswers
+     * @param \Illuminate\Http\Request $request
+     * @param mixed $examId
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
     public function submitAnswers(Request $request, $examId)
     {
         $user = Auth::user();
